4c3c027e787648fa9ac0255ff6d961228eeba919
Mohammed Noori <kmanoorib@gmail.com>
2019-07-04 18:13:02 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,4 +27,9 @@ class PostsController < ApplicationController
 		post.update(params.require(:post))
 		redirect_to post_path(post)
 	end
+	
+	private
+	
+	def post_params
+	  params.require(:post).
 end

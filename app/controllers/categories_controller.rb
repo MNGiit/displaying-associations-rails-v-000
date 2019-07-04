21536885a8cbd3e3cef68b2ca1f591995f19f415
Mohammed Noori <kmanoorib@gmail.com>
2019-07-04 18:21:32 +0000

    Automatically backed up by Learn

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,4 +25,10 @@ class CategoriesController < ApplicationController
     category.update(params.require(:category))
     redirect_to category_path(category)
   end
+  
+  private
+  def post_params
+	  # params.require(:user).permit(:username, :email, :password, :salt, :encrypted_password)
+	  params.require(:post).permit(:title, :description, :category)
+	end
 end

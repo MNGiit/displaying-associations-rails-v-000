569047f886cf7c1af1148e1f54ccd8894d20ff87
Mohammed Noori <kmanoorib@gmail.com>
2019-07-04 18:16:27 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,8 +13,8 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		post = Post.create(params[:post])
-		redirect_to post_path(post)
+		@post = Post.create(params[:post])
+		redirect_to post_path(@post)
 	end
 
 	def edit
@@ -23,10 +23,10 @@ class PostsController < ApplicationController
 	end
 
 	def update
-		post = Post.find(params[:id])
+		@post = Post.find(params[:id])
 		# post.update(params.require(:post))
-		post.update(post_params)
-		redirect_to post_path(post)
+		@post.update(post_params)
+		redirect_to post_path(@post)
 	end
 	
 	private
